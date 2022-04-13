aff3c24d9c98a16a01808cb3f836bc3729ad6d46
Yuheng Cai <yhcai@umich.edu>
2022-04-13 09:08:38 -0500

    async write through node file system

---
+++ 03.file_system/async.js
@@ -0,0 +1,20 @@
+var fs = require("fs")
+fs.open("hello.txt", "w", function(err, fd) {
+        if(!err){
+            fs.write(fd, "this is written asynchronously", function(err){
+                if(!err){
+                    console.log("aync write succeeded!")
+                }
+                fs.close(fd, function(err){
+                    if(!err){
+                        console.log("file is closed!")
+                    }
+                })
+            })
+        }else{
+            console.log(err);
+        }
+    }
+)
+
+console.log("this line of code will be run before the asynchronous code")
